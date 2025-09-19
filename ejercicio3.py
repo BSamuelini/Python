fc6eab983c3dac8918965c0a2b0f89d622b0f4f0
Samuelin <102695634+BSamuelini@users.noreply.github.com>
2025-09-19 08:55:54 +0200

    Add files via upload

---
+++ ejercicio3.py
@@ -0,0 +1,6 @@
+# Crea un programa llamado Saludo.py que le pregunte al usuario su nombre y edad, y muestre un mensaje de saludo personalizado, de este modo
+
+nombre = input("¿Cuál es tu nombre? ")
+edad = int(input("¿Cuántos años tienes? "))
+
+print(f"\nHola {nombre}, tienes {edad} años.")
